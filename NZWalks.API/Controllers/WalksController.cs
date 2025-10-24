7c071e0adc5e529071ffcd2a15dc0eac60e08257
Haphanswsw <haphan.sw@gmail.com>
2025-10-24 22:06:08 +0700

    add filter, sort and pagination

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -22,11 +22,13 @@ namespace NZWalks.API.Controllers
             this.walkRepository = walkRepository;
         }
         // GET ALL Walks
-        // GET: /api/walks
+        // GET: /api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery, 
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
         {
-            var walkDomain = await walkRepository.GetAllAsync();
+            var walkDomain = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy,
+                                                                isAscending ?? true, pageNumber, pageSize);
             return Ok(mapper.Map<List<WalkDto>>(walkDomain));
         }
         // GET SINGLE Walk
